5c16339da3b9e6c136b9ce2f28e919dda4f6ad73
Singh <swatika.singh@accenture.com>
2024-03-04 01:01:35 +0530

    functional and class component

--- App.js
+++ App.js
@@ -1,21 +1,40 @@
-const parent = React.createElement(
-    "div",
-{id : "parent"},[
-React.createElement(
-    "div",
-     { id: "child"},[
-    React.createElement("h1",{},"i am h1 tag"),
-    React.createElement("h2",{},"i am h2 tag"),
-]),
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-React.createElement("div", { id: "child2"},[
-    React.createElement("h1",{},"i am h1 tag"),
-    React.createElement("h2",{},"i am h2 tag"),
+// jsx => React.createElement => ReactElement.Js object => htmlElelement(render)
 
-]),
-]);
+/**
+ * header
+ * -logo
+ * -nav items
+ * Body
+ * -search
+ * -restaurantContainer
+ * -RestaurantCard
+ * Footer
+ * ----copyright
+ * -----Links
+ * -----Address
+ * -----Contact
+ *
+ */
 
-    console.log(parent);
-        const root = ReactDOM.createRoot(document.getElementById("root"));
+//fuctional component react
+const Title = () => (
+  <h1 className="head" tabIndex="5">
+    Namaste React using jsx
+  </h1>
+);
 
-        root.render(parent);
+//Component compostion
+const HeadingComponent = () => (
+  <div id="container">
+    <Title></Title>
+
+    <h1 className="heading">Namaste React using fuctional component</h1>
+  </div>
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<HeadingComponent />);
